6455ae7d6b54afa89a1193760846dd83a621a6ef
Nick Alexander <alexander50@marshall.edu>
2015-05-01 00:13:23 -0400

    Add Database High Stakes to 410

---
+++ 410/High Stakes/SQL.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE THEATRE;
+
+CREATE DOMAIN GENDER CHAR(1) (
+	CHECK(
+		value IN (
+			'M', 'F'
+		)
+	)
+);
+
+CREATE DOMAIN GENDER CHAR(1)
+	CHECK (value IN (
+		'M', 'F'
+	));
+
+CREATE TABLE movie (
+	movie_id	SERIAL PRIMARY KEY,
+	title		VARCHAR(255) NOT NULL,
+	year		INT NOT NULL
+);
+
+CREATE TABLE actor (
+	actor_id			SERIAL PRIMARY KEY,
+	name				VARCHAR(255),
+	gender				GENDER,
+	movie_id			INT REFERENCES movie(movie_id),
+	character_name		VARCHAR(255)
+);
+
+CREATE TABLE producer (
+	producer_id SERIAL PRIMARY KEY,
+	name		VARCHAR(255),
+	movie_id	INT REFERENCES movie(movie_id),
+);
+
+CREATE TABLE director (
+	director_id SERIAL PRIMARY KEY,
+	name VARCHAR(255),
+	movie_id INT REFERENCES movie(movie_id)
+);
+
+CREATE TABLE theatre (
+	theatre_id SERIAL PRIMARY KEY,
+	theatre_name VARCHAR(255),
+	showrooms INT,
+	city VARCHAR(255),
+	state CHAR(2),
+	zipcode CHAR(5)
+);
+
+CREATE TABLE showtime (
+	showtime_id SERIAL PRIMARY KEY,
+	theatre_id INT REFERENCES theatre(theatre_id),
+	movie_id INT REFERENCES movie(movie_id),
+	start_time TIME,
+	end_time TIME,
+	show_date DATE
+);
