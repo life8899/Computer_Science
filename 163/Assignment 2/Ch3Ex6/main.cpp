5f4f955e4f798cfe08f63d882a2110f40fdcdc40
Nick Alexander <alexander50@marshall.edu>
2015-05-19 18:56:51 -0400

    As2Ch3Ex6 - implement output file write.

--- 163/Assignment 2/Ch3Ex6/main.cpp
+++ 163/Assignment 2/Ch3Ex6/main.cpp
@@ -10,7 +10,7 @@ using namespace std;
  *
  * Prompts user for path to data file containing employee information.
  * Formats and displays employee first name, last name, and salary after
- * raise.
+ * raise. Information is also written to an output file specified by the user.
  *
  * Note: data file must respect the following format:
  *      lastName -space- firstName -space- Salary -space- PercentRaise
@@ -28,7 +28,14 @@ int main()
 	getline(cin, dataFilePath); // Get data file path from user
 
 	ifstream dataFile;
-	dataFile.open(dataFilePath);
+	dataFile.open(dataFilePath); // Open data file for read
+
+	string outputFilePath;
+	cout << "Path to Output File: ";
+	getline(cin, outputFilePath); // Get output file path from user
+
+	ofstream outputFile;
+	outputFile.open(outputFilePath); // Open output file for write
 
 	const int colWidth = 10;
 	cout << fixed << showpoint << setprecision(2); // format to 2 decimal places
@@ -49,7 +56,11 @@ int main()
 		float updatedSalary = currentSalary + (currentSalary * (percentRaise/100)); // Calculate new salary
 
 		cout << left << setw(colWidth) << firstName << setw(colWidth) << lastName << setw(colWidth) << updatedSalary << endl;
+		outputFile << left << setw(colWidth) << firstName << setw(colWidth) << lastName << setw(colWidth) << updatedSalary << endl;
 	}
+
+	dataFile.close();
+	outputFile.close(); // Close files
 
 	return 0;
 }
